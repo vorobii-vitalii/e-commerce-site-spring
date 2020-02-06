30bbd104e713fc41cd3b59ae5ddfa0a7b3e10ef1
vitalii <vitalii.vorobij@gmail.com>
2020-02-06 20:09:17 +0200

    Added Spring Boot start-up class.

---
+++ src/main/java/com/commerce/web/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
