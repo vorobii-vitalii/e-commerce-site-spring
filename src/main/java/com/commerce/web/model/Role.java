b4aed92a410a87450094a82b3f04135a73cebb4a
vitalii <vitalii.vorobij@gmail.com>
2020-02-06 19:26:28 +0200

    Added Role and User models, which are mapped Many-To-Many

---
+++ src/main/java/com/commerce/web/model/Role.java
@@ -0,0 +1,18 @@
+package com.commerce.web.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name="roles")
+public class Role extends BaseEntity {
+
+    @Column(name="name")
+    private String name;
+
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
+    private List<User> users;
+}
