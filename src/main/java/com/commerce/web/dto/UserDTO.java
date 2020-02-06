570b73418c6772624877f1641191a5a2f96f205c
vitalii <vitalii.vorobij@gmail.com>
2020-02-06 20:04:50 +0200

    Added User DTO model to distribute precise information in REST service later on

---
+++ src/main/java/com/commerce/web/dto/UserDTO.java
@@ -0,0 +1,37 @@
+package com.commerce.web.dto;
+
+import com.commerce.web.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User toUser() {
+        User user = new User ();
+        user.setId ( id );
+        user.setUsername ( username );
+        user.setFirstName ( firstName );
+        user.setLastName ( lastName );
+        user.setEmail ( email );
+
+        return user;
+    }
+
+    public static UserDTO fromUser(User user) {
+        UserDTO userDTO = new UserDTO ();
+        userDTO.setId ( user.getId ( ) );
+        userDTO.setEmail ( user.getEmail () );
+        userDTO.setFirstName ( user.getFirstName () );
+        userDTO.setLastName ( user.getLastName () );
+        userDTO.setUsername ( user.getUsername () );
+
+        return userDTO;
+    }
+}
