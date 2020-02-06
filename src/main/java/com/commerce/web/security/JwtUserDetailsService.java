0c8c60f8a90561796d1dd16dce217e0c55e68131
vitalii <vitalii.vorobij@gmail.com>
2020-02-06 19:59:53 +0200

    Implemented custom Jwt Details service, in order to override logic of loading user.

---
+++ src/main/java/com/commerce/web/security/JwtUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.commerce.web.security;
+
+import com.commerce.web.model.User;
+import com.commerce.web.security.jwt.JwtUser;
+import com.commerce.web.security.jwt.JwtUserFactory;
+import com.commerce.web.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Autowired
+    public JwtUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername ( String username ) throws UsernameNotFoundException {
+        User user = userService.findByUsername ( username );
+
+        if( user == null ) {
+            throw new UsernameNotFoundException ( "User with username " + username + " not found..." );
+        }
+
+        JwtUser jwtUser = JwtUserFactory.create ( user );
+        log.info ( "loadByUserName - user with username {} succesfully loaded", username );
+
+        return jwtUser;
+    }
+}
