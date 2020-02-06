291ad82f41b52a6d0ac855a3ecbcc3dc853521a7
vitalii <vitalii.vorobij@gmail.com>
2020-02-06 19:24:31 +0200

    Added Mappaed super-class BaseEntity that contains common properties for all models in application

---
+++ src/main/java/com/commerce/web/model/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.commerce.web.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@Data
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name="created")
+    private Date created;
+
+    @LastModifiedDate
+    @Column(name="updated")
+    private Date updated;
+
+    @Column(name="status")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+}
