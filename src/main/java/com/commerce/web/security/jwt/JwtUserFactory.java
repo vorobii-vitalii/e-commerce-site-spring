c1ae3e87bb24b37df6b3bebed490ff9f5a3ca365
vitalii <vitalii.vorobij@gmail.com>
2020-02-06 19:50:21 +0200

    Implemented Factory Design pattern for JwtUser objects creating

---
+++ src/main/java/com/commerce/web/security/jwt/JwtUserFactory.java
@@ -0,0 +1,39 @@
+package com.commerce.web.security.jwt;
+
+import com.commerce.web.model.Role;
+import com.commerce.web.model.Status;
+import com.commerce.web.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public final class JwtUserFactory {
+    public JwtUserFactory() {
+
+    }
+
+    public static JwtUser create( User user) {
+        return new JwtUser (
+                user.getId (),
+                user.getUsername (),
+                user.getFirstName (),
+                user.getLastName (),
+                user.getEmail (),
+                user.getPassword (),
+                mapToGrantedAuthorities ( user.getRoles () ),
+                user.getStatus ().equals ( Status.ACTIVE ),
+                user.getUpdated ()
+        );
+    }
+
+    private static List<GrantedAuthority> mapToGrantedAuthorities( List<Role> userRoles) {
+        return userRoles
+                .stream ()
+                .map( role ->
+                        new SimpleGrantedAuthority(role.getName ())
+                )
+                .collect ( Collectors.toList ( ) );
+    }
+}
