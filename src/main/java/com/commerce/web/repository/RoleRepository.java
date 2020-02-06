17ba847e3bbc7ac04b81dbf09737ed98b050b81b
vitalii <vitalii.vorobij@gmail.com>
2020-02-06 19:32:26 +0200

    Added Role and User repositories

---
+++ src/main/java/com/commerce/web/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.commerce.web.repository;
+
+import com.commerce.web.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Role findByName(String name);
+}
---
+++ src/main/java/com/commerce/web/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.commerce.web.repository;
+
+import com.commerce.web.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByUsername(String name);
+}
