b378890b83f9db73dd425f9fec4bd12d6dd1c4dc
vitalii <vitalii.vorobij@gmail.com>
2020-02-09 21:48:06 +0200

    Implemented mentioned before methods

--- src/main/java/com/commerce/web/service/implementation/UserServiceImplementation.java
+++ src/main/java/com/commerce/web/service/implementation/UserServiceImplementation.java
@@ -1,10 +1,13 @@
 package com.commerce.web.service.implementation;
 
-import com.commerce.web.model.Role;
-import com.commerce.web.model.Status;
-import com.commerce.web.model.User;
+import com.commerce.web.exceptions.UserIsAlreadyVerifiedException;
+import com.commerce.web.exceptions.UserWasNotFoundByEmailException;
+import com.commerce.web.exceptions.VerificationTokenExpiredException;
+import com.commerce.web.exceptions.VerificationTokenHasNotMatchedException;
+import com.commerce.web.model.*;
 import com.commerce.web.repository.RoleRepository;
 import com.commerce.web.repository.UserRepository;
+import com.commerce.web.repository.UserVerificationRepository;
 import com.commerce.web.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,19 +15,28 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
 public class UserServiceImplementation implements UserService {
 
     private final UserRepository userRepository;
+    private final UserVerificationRepository userVerificationRepository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImplementation(UserRepository userRepository,RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
+    public UserServiceImplementation(
+                UserRepository userRepository,
+                UserVerificationRepository userVerificationRepository,
+                RoleRepository roleRepository,
+                BCryptPasswordEncoder passwordEncoder
+    ) {
         this.userRepository = userRepository;
+        this.userVerificationRepository = userVerificationRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
@@ -33,6 +45,7 @@ public class UserServiceImplementation implements UserService {
     public User register ( User user ) {
 
         Role roleCustomer = roleRepository.findByName ( "ROLE_CUSTOMER" );
+
         List<Role> userRoles = new ArrayList<> ();
 
         // Attach customer role to new user
@@ -40,15 +53,80 @@ public class UserServiceImplementation implements UserService {
 
         // Encode the user's password
         user.setPassword(passwordEncoder.encode ( user.getPassword() ));
+
+        // Attach roles to user
         user.setRoles(userRoles);
 
-        // By default the new user is active
-        user.setStatus( Status.ACTIVE );
+        // By default the new user is not active (has to be verified)
+        user.setStatus( Status.NOT_ACTIVE );
 
-        // Save user in database
         User registeredUser = userRepository.save(user);
+
+        // Generate random UUID
+        UUID randomUUID = UUID.randomUUID ();
+
+        UserVerification userVerification = userVerificationRepository.save( UserVerificationFactory.create (
+                registeredUser, randomUUID.toString (), Status.ACTIVE
+        ));
+
+        registeredUser.setUserVerification ( userVerification );
+
+        userRepository.save ( registeredUser );
+
         log.info("IN REGISTER use {} successfully registered", registeredUser);
         return registeredUser;
+    }
+
+    @Override
+    public User verifyByToken ( String token ) throws VerificationTokenExpiredException, VerificationTokenHasNotMatchedException {
+
+        UserVerification foundUserVerification = userVerificationRepository.findByToken ( token );
+
+        if (foundUserVerification == null) {
+            throw new VerificationTokenHasNotMatchedException ( "Token wasn't found" );
+        }
+
+        Long tokenExpirationTime = foundUserVerification.getCreated ().getTime ();
+        Date validatedBy = new Date(tokenExpirationTime + 3600 * 2);
+
+        if ( ! new Date ().before ( validatedBy)  ) {
+            throw new VerificationTokenExpiredException ( "Token has expired" );
+        }
+
+        User matchedUser = foundUserVerification.getUser ();
+        matchedUser.setStatus ( Status.ACTIVE );
+
+        log.info ( "Verified user {} by token {}",matchedUser,token);
+
+        userRepository.save ( matchedUser );
+
+        return matchedUser;
+    }
+
+    @Override
+    public String regenerateToken ( String email ) throws UserWasNotFoundByEmailException, UserIsAlreadyVerifiedException {
+
+        User user = userRepository.findByEmail ( email );
+
+        if (user == null) {
+            throw new UserWasNotFoundByEmailException ( "User wasn't found by email: " + email );
+        }
+
+        if (user.getStatus () == Status.ACTIVE) {
+            throw new UserIsAlreadyVerifiedException ( "User with email " + email + " is verified!" );
+        }
+
+        UserVerification userVerification = user.getUserVerification ();
+
+        String newToken = UUID.randomUUID ().toString ();
+
+        userVerification.setToken ( newToken );
+
+        userVerificationRepository.save(userVerification);
+
+        log.info ( "Generated new token for {} ", user );
+
+        return newToken;
     }
 
     @Override
@@ -59,9 +137,9 @@ public class UserServiceImplementation implements UserService {
     }
 
     @Override
-    public User findByUsername ( String username ) {
-        User foundUser = userRepository.findByUsername ( username );
-        //log.info ( "findByUsername {}", foundUser );
+    public User findByEmail ( String email ) {
+        User foundUser = userRepository.findByEmail ( email );
+        log.info ( "findByEmail {}", foundUser );
         return foundUser;
     }
 
@@ -71,6 +149,8 @@ public class UserServiceImplementation implements UserService {
         log.info ( "findById {}", foundUser );
         return foundUser;
     }
+
+
 
     @Override
     public void delete ( Long id ) {
