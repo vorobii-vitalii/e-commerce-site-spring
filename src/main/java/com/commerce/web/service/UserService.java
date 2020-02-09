9518b958dbdc5a665869cf275204dfe196e68e48
vitalii <vitalii.vorobij@gmail.com>
2020-02-09 21:47:26 +0200

    Added verifyByToken, findByEmail, regenerateTokem opportunities in UserService interface

--- src/main/java/com/commerce/web/service/UserService.java
+++ src/main/java/com/commerce/web/service/UserService.java
@@ -1,5 +1,9 @@
 package com.commerce.web.service;
 
+import com.commerce.web.exceptions.UserIsAlreadyVerifiedException;
+import com.commerce.web.exceptions.UserWasNotFoundByEmailException;
+import com.commerce.web.exceptions.VerificationTokenExpiredException;
+import com.commerce.web.exceptions.VerificationTokenHasNotMatchedException;
 import com.commerce.web.model.User;
 
 import java.util.List;
@@ -8,11 +12,15 @@ public interface UserService {
 
     User register( User user);
 
+    User verifyByToken(String token) throws VerificationTokenExpiredException, VerificationTokenHasNotMatchedException;
+
     List<User> getAll();
 
-    User findByUsername(String username);
+    User findByEmail(String email);
 
     User findById(Long id);
+
+    String regenerateToken(String email) throws UserWasNotFoundByEmailException, UserIsAlreadyVerifiedException;
 
     void delete(Long id);
 }
