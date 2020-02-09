982b8039799de641d63e859c922f0b6427009fad
vitalii <vitalii.vorobij@gmail.com>
2020-02-09 21:44:31 +0200

    Updated UserRepository

--- src/main/java/com/commerce/web/repository/UserRepository.java
+++ src/main/java/com/commerce/web/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.commerce.web.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByUsername(String name);
+    User findByEmail(String email);
 }
