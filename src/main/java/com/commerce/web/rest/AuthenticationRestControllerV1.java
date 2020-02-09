edd11117f86a049241c24966eb8f7570ea94709e
vitalii <vitalii.vorobij@gmail.com>
2020-02-09 21:49:16 +0200

    Implemented REST API endpoints for user verification and token resending.

--- src/main/java/com/commerce/web/rest/AuthenticationRestControllerV1.java
+++ src/main/java/com/commerce/web/rest/AuthenticationRestControllerV1.java
@@ -1,13 +1,23 @@
 package com.commerce.web.rest;
 
+import com.commerce.web.constants.SiteConstants;
 import com.commerce.web.dto.AuthenticationRequestDTO;
 import com.commerce.web.dto.RegistrationRequestDTO;
+import com.commerce.web.dto.UserDTO;
+import com.commerce.web.dto.VerifyAccountDTO;
+import com.commerce.web.exceptions.UserIsAlreadyVerifiedException;
+import com.commerce.web.exceptions.UserWasNotFoundByEmailException;
+import com.commerce.web.exceptions.VerificationTokenExpiredException;
+import com.commerce.web.exceptions.VerificationTokenHasNotMatchedException;
+import com.commerce.web.mail_templates.AccountValidationMailMessage;
+import com.commerce.web.model.Status;
 import com.commerce.web.model.User;
-import com.commerce.web.security.jwt.JwtAuthenthicationException;
 import com.commerce.web.security.jwt.JwtTokenProvider;
 import com.commerce.web.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -26,6 +36,9 @@ public class AuthenticationRestControllerV1 {
     private AuthenticationManager authenticationManager;
 
     @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
     @Autowired
@@ -41,58 +54,88 @@ public class AuthenticationRestControllerV1 {
         try {
             registeredUser = userService.register ( providedUser );
         }
-        catch (Exception e) { }
+        catch (Exception ignored) { }
 
-        if (registeredUser == null) {
+        if ( registeredUser == null ) {
             throw new BadCredentialsException ( "Invalid credentials" );
         }
 
-        String username = registeredUser.getUsername ();
+        String email = registeredUser.getEmail ();
+        String token = registeredUser.getUserVerification ().getToken ();
 
-        String token = jwtTokenProvider.createToken (
-                username,
-                registeredUser.getRoles ()
-        );
+        // Send email to person
+        this.mailSender.send ( new AccountValidationMailMessage(email,token, SiteConstants.BASE_URL ).generate () );
 
-        Map<Object,Object> response = new HashMap<> ();
-        response.put ( "username" , username );
-        response.put ( "token" , token );
-
-        return ResponseEntity.ok ( response );
+        return (ResponseEntity) ResponseEntity.ok ( );
     }
 
     @PostMapping(value = "login")
     public ResponseEntity login( @RequestBody AuthenticationRequestDTO requestDTO ) {
+
         try {
-            System.out.println ( "REQUEST : "+ requestDTO.getUsername () + " : "+ requestDTO.getPassword () );
-            String username = requestDTO.getUsername ();
+
+            String email = requestDTO.getEmail ();
+
             authenticationManager.authenticate (
                     new UsernamePasswordAuthenticationToken (
-                            username,
+                            email,
                             requestDTO.getPassword ()
                     )
             );
-            User user = userService.findByUsername ( username );
 
-            if(user == null) {
-                throw new UsernameNotFoundException ( "User with username " + username + " not found");
+            User user = userService.findByEmail ( email );
+
+            if( user == null ) {
+                throw new UsernameNotFoundException ( "User with email " + email + " not found");
+            }
+            else if( user.getStatus () != Status.ACTIVE ) {
+                throw new BadCredentialsException ( "User with email " + email + " is not verified or deleted");
             }
 
-
-            String token = jwtTokenProvider.createToken ( username, user.getRoles () );
+            String token = jwtTokenProvider.createToken ( email, user.getRoles () );
 
             Map<Object,Object> response = new HashMap<> ();
-            response.put ( "username" , username );
+            response.put ( "email" , email );
             response.put ( "token" , token );
 
             return ResponseEntity.ok ( response );
         }
         catch(AuthenticationException  e) {
-            throw new BadCredentialsException ( "Invalid username or password" );
-        }
-        catch(Exception e) {
-            System.out.println ( "Something wrong happaned with LOGIN" );
-            return null;
+            throw new BadCredentialsException ( "Invalid email or password" );
         }
     }
+
+    @GetMapping(value = "verify/{token}")
+    public ResponseEntity<UserDTO> verify( @PathVariable(name="token") String token) throws VerificationTokenExpiredException {
+
+        try {
+            User verifiedUser = userService.verifyByToken ( token );
+
+            return ResponseEntity.ok ( UserDTO.fromUser ( verifiedUser) );
+        }
+        catch(VerificationTokenExpiredException | VerificationTokenHasNotMatchedException e) {
+            throw new BadCredentialsException ( e.getMessage () );
+        }
+
+    }
+
+    @PostMapping(value = "resend")
+    public ResponseEntity resendToken( @RequestBody VerifyAccountDTO verifyAccountDTO ) throws UserIsAlreadyVerifiedException, UserWasNotFoundByEmailException {
+
+        String email = verifyAccountDTO.getEmail ();
+
+        try {
+            String token = userService.regenerateToken ( email );
+
+            // Resend token to person
+            this.mailSender.send ( new AccountValidationMailMessage(email,token, SiteConstants.BASE_URL ).generate () );
+
+            return (ResponseEntity) ResponseEntity.ok ( );
+        }
+        catch(UserIsAlreadyVerifiedException | UserWasNotFoundByEmailException e) {
+            throw new BadCredentialsException ( e.getMessage () );
+        }
+
+    }
+
 }
