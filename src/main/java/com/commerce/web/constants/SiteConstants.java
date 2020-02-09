54011c1eb2ffae70fc3fadcbc4cd76801b78dd3e
vitalii <vitalii.vorobij@gmail.com>
2020-02-09 21:40:41 +0200

    Added some useful constants classes

---
+++ src/main/java/com/commerce/web/constants/SiteConstants.java
@@ -0,0 +1,5 @@
+package com.commerce.web.constants;
+
+public class SiteConstants {
+    public static String BASE_URL = "http://localhost:8167";
+}
---
+++ src/main/java/com/commerce/web/constants/UserRolesConstants.java
@@ -0,0 +1,8 @@
+package com.commerce.web.constants;
+
+public class UserRolesConstants {
+
+    public static String ADMIN = "ADMIN";
+    public static String CUSTOMER = "CUSTOMER";
+
+}
