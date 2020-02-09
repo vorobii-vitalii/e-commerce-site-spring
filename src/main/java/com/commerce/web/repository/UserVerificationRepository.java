a317f1fa3f72ca380010bc1f39834b3f859ff9f5
vitalii <vitalii.vorobij@gmail.com>
2020-02-09 21:44:13 +0200

    Added UserVerificationRepository

---
+++ src/main/java/com/commerce/web/repository/UserVerificationRepository.java
@@ -0,0 +1,8 @@
+package com.commerce.web.repository;
+
+import com.commerce.web.model.UserVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserVerificationRepository extends JpaRepository<UserVerification,Long> {
+    UserVerification findByToken(String token);
+}
