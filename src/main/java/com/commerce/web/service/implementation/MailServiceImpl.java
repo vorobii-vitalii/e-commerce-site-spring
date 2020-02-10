46a0bf7e61f905069b68e6405b1a1ec68ba1669d
vitalii <vitalii.vorobij@gmail.com>
2020-02-10 16:19:29 +0200

    Added MailService implementation

---
+++ src/main/java/com/commerce/web/service/implementation/MailServiceImpl.java
@@ -0,0 +1,24 @@
+package com.commerce.web.service.implementation;
+
+import com.commerce.web.service.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailServiceImpl implements MailService {
+
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public MailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void send ( SimpleMailMessage simpleMailMessage ) {
+        this.mailSender.send ( simpleMailMessage );
+    }
+
+}
