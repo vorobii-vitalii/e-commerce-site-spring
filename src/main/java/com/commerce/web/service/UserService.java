38e1093b800c45641eb08b9d3794a461fc827a00
vitalii <vitalii.vorobij@gmail.com>
2020-02-10 23:46:55 +0200

    Updated UserService

--- src/main/java/com/commerce/web/service/UserService.java
+++ src/main/java/com/commerce/web/service/UserService.java
@@ -1,9 +1,6 @@
 package com.commerce.web.service;
 
-import com.commerce.web.exceptions.UserIsAlreadyVerifiedException;
-import com.commerce.web.exceptions.UserWasNotFoundByEmailException;
-import com.commerce.web.exceptions.VerificationTokenExpiredException;
-import com.commerce.web.exceptions.VerificationTokenHasNotMatchedException;
+import com.commerce.web.exceptions.*;
 import com.commerce.web.model.User;
 
 import java.util.List;
@@ -12,15 +9,19 @@ public interface UserService {
 
     User register( User user);
 
-    User verifyByToken(String token) throws VerificationTokenExpiredException, VerificationTokenHasNotMatchedException;
-
     List<User> getAll();
 
     User findByEmail(String email);
 
     User findById(Long id);
 
-    String regenerateToken(String email) throws UserWasNotFoundByEmailException, UserIsAlreadyVerifiedException;
+    User verifyByToken(String token) throws VerificationTokenExpiredException, VerificationTokenHasNotMatchedException;
+
+    String regenerateVerificationToken ( String email) throws UserWasNotFoundByEmailException, UserIsAlreadyVerifiedException;
+
+    String resetPasswordByEmail(String email) throws UserWasNotFoundByEmailException, UserNotActiveException;
+
+    void changePasswordByToken(String token,String password) throws PasswordResetTokenIsNotValid, VerificationTokenExpiredException, PasswordResetTokenIsNotActive, PasswordResetTokenHasExpired;
 
     void delete(Long id);
 }
