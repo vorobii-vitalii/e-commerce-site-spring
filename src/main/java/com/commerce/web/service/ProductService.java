cab7618713e137fd9c8996cdcbf57f76e1c449b0
vitalii <vitalii.vorobij@gmail.com>
2020-02-16 23:06:24 +0200

    Updated ProductService

--- src/main/java/com/commerce/web/service/ProductService.java
+++ src/main/java/com/commerce/web/service/ProductService.java
@@ -1,12 +1,18 @@
 package com.commerce.web.service;
 
+import com.commerce.web.dto.AddProductRequestDTO;
+import com.commerce.web.dto.ProductDTO;
 import com.commerce.web.exceptions.ProductNotFoundException;
 import com.commerce.web.exceptions.ProductsResultIsEmptyException;
+import com.commerce.web.exceptions.SpecificationNotFoundByNameException;
 import com.commerce.web.model.Product;
+import com.commerce.web.model.User;
 
 import java.util.List;
 
 public interface ProductService {
+
+    void addProduct( AddProductRequestDTO addProductRequestDTO, User author ) throws SpecificationNotFoundByNameException;
 
     List<Product> getAll() throws ProductsResultIsEmptyException;
 
