d6c40127b2379cf544c64e8fd228cc769ebbeda2
vitalii <vitalii.vorobij@gmail.com>
2020-02-16 21:40:55 +0200

    Created SpecificationService interface

---
+++ src/main/java/com/commerce/web/service/SpecificationService.java
@@ -0,0 +1,20 @@
+package com.commerce.web.service;
+
+import com.commerce.web.dto.EditSpecificationDTO;
+import com.commerce.web.dto.SpecificationDTO;
+import com.commerce.web.exceptions.SpecificationNameIsTakenException;
+import com.commerce.web.exceptions.SpecificationNotFoundByNameException;
+import com.commerce.web.exceptions.SpecificationNotFoundException;
+import com.commerce.web.model.Specification;
+
+public interface SpecificationService {
+
+    void addSpecification( SpecificationDTO specificationDTO ) throws SpecificationNameIsTakenException;
+
+    SpecificationDTO getByName(String name) throws SpecificationNotFoundByNameException;
+
+    void editById( Long id, EditSpecificationDTO editSpecificationDTO ) throws SpecificationNotFoundException, SpecificationNameIsTakenException;
+
+    void deleteById(Long id) throws SpecificationNotFoundException;
+
+}
