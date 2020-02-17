a3d9696cde1a566faf4b317fe131607561d24b0e
vitalii <vitalii.vorobij@gmail.com>
2020-02-17 11:24:01 +0200

    Added ProductSpecificationRepository

---
+++ src/main/java/com/commerce/web/repository/ProductSpecificationRepository.java
@@ -0,0 +1,7 @@
+package com.commerce.web.repository;
+
+import com.commerce.web.model.ProductSpecification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductSpecificationRepository extends JpaRepository<ProductSpecification,Long> {
+}
